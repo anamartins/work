53ade845215255bebb6a337715ffaff3c69b27b5
Ana Martins <anacarolcm@gmail.com>
2020-10-30 18:26:04 +0100

    Add the spaceship and the chain

--- src/components/astronaut/astronaut.js
+++ src/components/astronaut/astronaut.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef } from "react";
-import Matter, { Common, MouseConstraint } from "matter-js";
+import Matter, { Common, MouseConstraint, Composites, Runner } from "matter-js";
 
 import "./style.scss";
 
@@ -11,16 +11,18 @@ export default function Astronaut() {
   let Body = Matter.Body;
   let Constraint = Matter.Constraint;
   let Composite = Matter.Composite;
-  let Composites = Matter.Composites;
+  // let Composites = Matter.Composites;
 
   let World = Matter.World;
   let Render = Matter.Render;
   let Mouse = Matter.Mouse;
 
+  // let canvasWidth = (window.innerWidth * 2) / 3;
   let canvasWidth = window.innerWidth;
-  let canvasHeight = 250;
 
-  let x = 800;
+  let canvasHeight = 500;
+
+  let x = 700;
   let y = 100;
 
   let scale = 1;
@@ -34,10 +36,6 @@ export default function Astronaut() {
 
     initPhysics(canvas);
     initAstronaut();
-
-    // setInterval(() => {
-    //   drawAstronaut(ctx);
-    // }, 1000 / 30);
   }
 
   function initPhysics(canvas) {
@@ -47,22 +45,23 @@ export default function Astronaut() {
       options: {
         width: canvasWidth,
         height: canvasHeight,
-        // wireframes: true,
+        wireframes: false,
       },
     });
     Engine.run(engine);
     Render.run(render);
-    engine.world.gravity.y = 0;
+    engine.world.gravity.y = -0.003;
+    engine.world.gravity.x = -0.001;
 
-    render.options.wireframeBackground = "transparent";
+    // render.options.wireframeBackground = "transparent";
 
     initMouse(render);
 
-    // render.options.background = "transparent";
+    render.options.background = "transparent";
   }
 
   function initAstronaut() {
-    var headOptions = Common.extend({
+    let headOptions = Common.extend({
       label: "head",
       collisionFilter: {
         group: Body.nextGroup(true),
@@ -75,7 +74,7 @@ export default function Astronaut() {
       },
     });
 
-    var chestOptions = Common.extend({
+    let chestOptions = Common.extend({
       label: "chest",
       collisionFilter: {
         group: Body.nextGroup(true),
@@ -88,7 +87,7 @@ export default function Astronaut() {
       },
     });
 
-    var leftArmOptions = Common.extend({
+    let leftArmOptions = Common.extend({
       label: "left-arm",
       collisionFilter: {
         group: Body.nextGroup(true),
@@ -101,14 +100,14 @@ export default function Astronaut() {
       },
     });
 
-    var leftLowerArmOptions = Common.extend({}, leftArmOptions, {
+    let leftLowerArmOptions = Common.extend({}, leftArmOptions, {
       label: "left-arm-lower",
       render: {
         fillStyle: "#E59B12",
       },
     });
 
-    var rightArmOptions = Common.extend({
+    let rightArmOptions = Common.extend({
       label: "right-arm",
       collisionFilter: {
         group: Body.nextGroup(true),
@@ -121,14 +120,14 @@ export default function Astronaut() {
       },
     });
 
-    var rightLowerArmOptions = Common.extend({}, rightArmOptions, {
+    let rightLowerArmOptions = Common.extend({}, rightArmOptions, {
       label: "right-arm-lower",
       render: {
         fillStyle: "#E59B12",
       },
     });
 
-    var leftLegOptions = Common.extend({
+    let leftLegOptions = Common.extend({
       label: "left-leg",
       collisionFilter: {
         group: Body.nextGroup(true),
@@ -141,14 +140,14 @@ export default function Astronaut() {
       },
     });
 
-    var leftLowerLegOptions = Common.extend({}, leftLegOptions, {
+    let leftLowerLegOptions = Common.extend({}, leftLegOptions, {
       label: "left-leg-lower",
       render: {
         fillStyle: "#E59B12",
       },
     });
 
-    var rightLegOptions = Common.extend({
+    let rightLegOptions = Common.extend({
       label: "right-leg",
       collisionFilter: {
         group: Body.nextGroup(true),
@@ -161,71 +160,71 @@ export default function Astronaut() {
       },
     });
 
-    var rightLowerLegOptions = Common.extend({}, rightLegOptions, {
+    let rightLowerLegOptions = Common.extend({}, rightLegOptions, {
       label: "right-leg-lower",
       render: {
         fillStyle: "#E59B12",
       },
     });
 
-    var head = Bodies.rectangle(
+    let head = Bodies.rectangle(
       x,
       y - 60 * scale,
       34 * scale,
       40 * scale,
       headOptions
     );
-    var chest = Bodies.rectangle(x, y, 55 * scale, 80 * scale, chestOptions);
-    var rightUpperArm = Bodies.rectangle(
+    let chest = Bodies.rectangle(x, y, 55 * scale, 80 * scale, chestOptions);
+    let rightUpperArm = Bodies.rectangle(
       x + 39 * scale,
       y - 15 * scale,
       20 * scale,
       40 * scale,
       rightArmOptions
     );
-    var rightLowerArm = Bodies.rectangle(
+    let rightLowerArm = Bodies.rectangle(
       x + 39 * scale,
       y + 25 * scale,
       20 * scale,
       60 * scale,
       rightLowerArmOptions
     );
-    var leftUpperArm = Bodies.rectangle(
+    let leftUpperArm = Bodies.rectangle(
       x - 39 * scale,
       y - 15 * scale,
       20 * scale,
       40 * scale,
       leftArmOptions
     );
-    var leftLowerArm = Bodies.rectangle(
+    let leftLowerArm = Bodies.rectangle(
       x - 39 * scale,
       y + 25 * scale,
       20 * scale,
       60 * scale,
       leftLowerArmOptions
     );
-    var leftUpperLeg = Bodies.rectangle(
+    let leftUpperLeg = Bodies.rectangle(
       x - 20 * scale,
       y + 57 * scale,
       20 * scale,
       40 * scale,
       leftLegOptions
     );
-    var leftLowerLeg = Bodies.rectangle(
+    let leftLowerLeg = Bodies.rectangle(
       x - 20 * scale,
       y + 97 * scale,
       20 * scale,
       60 * scale,
       leftLowerLegOptions
     );
-    var rightUpperLeg = Bodies.rectangle(
+    let rightUpperLeg = Bodies.rectangle(
       x + 20 * scale,
       y + 57 * scale,
       20 * scale,
       40 * scale,
       rightLegOptions
     );
-    var rightLowerLeg = Bodies.rectangle(
+    let rightLowerLeg = Bodies.rectangle(
       x + 20 * scale,
       y + 97 * scale,
       20 * scale,
@@ -233,7 +232,7 @@ export default function Astronaut() {
       rightLowerLegOptions
     );
 
-    var chestToRightUpperArm = Constraint.create({
+    let chestToRightUpperArm = Constraint.create({
       bodyA: chest,
       pointA: {
         x: 24 * scale,
@@ -244,11 +243,13 @@ export default function Astronaut() {
         y: -8 * scale,
       },
       bodyB: rightUpperArm,
-      stiffness: 0.6,
+      stiffness: 1,
     });
 
-    var chestToLeftUpperArm = Constraint.create({
+    let chestToLeftUpperArm = Constraint.create({
       bodyA: chest,
+      bodyB: leftUpperArm,
+
       pointA: {
         x: -24 * scale,
         y: -23 * scale,
@@ -257,11 +258,10 @@ export default function Astronaut() {
         x: 0,
         y: -8 * scale,
       },
-      bodyB: leftUpperArm,
-      stiffness: 0.6,
+      stiffness: 1,
     });
 
-    var chestToLeftUpperLeg = Constraint.create({
+    let chestToLeftUpperLeg = Constraint.create({
       bodyA: chest,
       pointA: {
         x: -10 * scale,
@@ -272,10 +272,10 @@ export default function Astronaut() {
         y: -10 * scale,
       },
       bodyB: leftUpperLeg,
-      stiffness: 0.6,
+      stiffness: 1,
     });
 
-    var chestToRightUpperLeg = Constraint.create({
+    let chestToRightUpperLeg = Constraint.create({
       bodyA: chest,
       pointA: {
         x: 10 * scale,
@@ -286,10 +286,10 @@ export default function Astronaut() {
         y: -10 * scale,
       },
       bodyB: rightUpperLeg,
-      stiffness: 0.6,
+      stiffness: 1,
     });
 
-    var upperToLowerRightArm = Constraint.create({
+    let upperToLowerRightArm = Constraint.create({
       bodyA: rightUpperArm,
       bodyB: rightLowerArm,
       pointA: {
@@ -300,10 +300,10 @@ export default function Astronaut() {
         x: 0,
         y: -25 * scale,
       },
-      stiffness: 0.6,
+      stiffness: 1,
     });
 
-    var upperToLowerLeftArm = Constraint.create({
+    let upperToLowerLeftArm = Constraint.create({
       bodyA: leftUpperArm,
       bodyB: leftLowerArm,
       pointA: {
@@ -314,10 +314,10 @@ export default function Astronaut() {
         x: 0,
         y: -25 * scale,
       },
-      stiffness: 0.6,
+      stiffness: 1,
     });
 
-    var upperToLowerLeftLeg = Constraint.create({
+    let upperToLowerLeftLeg = Constraint.create({
       bodyA: leftUpperLeg,
       bodyB: leftLowerLeg,
       pointA: {
@@ -328,10 +328,10 @@ export default function Astronaut() {
         x: 0,
         y: -20 * scale,
       },
-      stiffness: 0.6,
+      stiffness: 1,
     });
 
-    var upperToLowerRightLeg = Constraint.create({
+    let upperToLowerRightLeg = Constraint.create({
       bodyA: rightUpperLeg,
       bodyB: rightLowerLeg,
       pointA: {
@@ -342,10 +342,10 @@ export default function Astronaut() {
         x: 0,
         y: -20 * scale,
       },
-      stiffness: 0.6,
+      stiffness: 1,
     });
 
-    var headContraint = Constraint.create({
+    let headConstraint = Constraint.create({
       bodyA: head,
       pointA: {
         x: 0,
@@ -356,18 +356,21 @@ export default function Astronaut() {
         y: -35 * scale,
       },
       bodyB: chest,
-      stiffness: 0.6,
+      stiffness: 1,
+      render: {
+        visible: false,
+      },
     });
 
-    var legToLeg = Constraint.create({
-      bodyA: leftLowerLeg,
-      bodyB: rightLowerLeg,
-      stiffness: 0.01,
-    });
+    // let legToLeg = Constraint.create({
+    //   bodyA: leftLowerLeg,
+    //   bodyB: rightLowerLeg,
+    //   stiffness: 0.02,
+    // });
 
-    var person = Composite.create({
+    let astronaut = Composite.create({
       bodies: [
-        chest,
+        // chest,
         head,
         leftLowerArm,
         leftUpperArm,
@@ -383,20 +386,98 @@ export default function Astronaut() {
         upperToLowerRightArm,
         chestToLeftUpperArm,
         chestToRightUpperArm,
-        headContraint,
+        headConstraint,
         upperToLowerLeftLeg,
         upperToLowerRightLeg,
         chestToLeftUpperLeg,
         chestToRightUpperLeg,
+        // legToLeg,
       ],
     });
+    World.add(engine.world, [astronaut]);
 
-    World.add(engine.world, [person]);
+    // console.log("c", upperToLowerLeftArm);
+    console.log("c", chest);
+    console.log("h", head);
+
+    console.log("a", astronaut);
+
+    let spaceshipOptions = Common.extend({
+      label: "spaceship",
+      isStatic: true,
+      collisionFilter: {
+        group: -2,
+      },
+
+      render: {
+        fillStyle: "#FFFFFF",
+      },
+    });
+
+    let spaceship = Bodies.rectangle(
+      x - 200 * scale,
+      y + 100 * scale,
+      15 * scale,
+      15 * scale,
+      spaceshipOptions
+    );
+
+    // World.add(engine.world, [spaceship]);
+    console.log("s", spaceship);
+
+    // World.add(engine.world, [square]);
+
+    var bridge = Composites.stack(60, 90, 5, 1, 0, 0, function (x, y) {
+      return Bodies.rectangle(x - 20, y, 53, 5, {
+        collisionFilter: {
+          // group: Body.nextGroup(true),
+          group: -2,
+        },
+        isStatic: false,
+
+        chamfer: 5,
+        density: 0.005,
+        frictionAir: 0.6,
+        render: {
+          fillStyle: "#575375",
+        },
+      });
+    });
+
+    Composites.chain(bridge, 0.3, 0, -0.3, 0, {
+      stiffness: 0.2,
+      length: 2,
+      render: {
+        visible: true,
+      },
+    });
+    console.log("br", bridge);
+
+    World.add(engine.world, [
+      bridge,
+      chest,
+      spaceship,
+
+      Constraint.create({
+        bodyA: spaceship,
+        bodyB: bridge.bodies[0],
+        pointB: { x: -25, y: 0 },
+        length: 0,
+        stiffness: 0.9,
+      }),
+      Constraint.create({
+        bodyA: chest,
+        bodyB: bridge.bodies[bridge.bodies.length - 1],
+        pointB: { x: 25, y: 0 },
+        length: 2,
+        stiffness: 0,
+      }),
+    ]);
   }
 
   function initMouse(render) {
     let mouse = Mouse.create(render.canvas);
-    console.log("mouse", mouse);
+    // console.log("mouse", mouse);
 
     let mouseConstraint = MouseConstraint.create(engine, {
       mouse: mouse,
